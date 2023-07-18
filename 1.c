900960ed26e7b1bfcdb25133c576015a6dcf922a
Divyaveer <86354945+Divyaveer@users.noreply.github.com>
2023-07-18 20:04:45 +0530

    first

---
+++ 1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+    printf("Name: Divyaveer.\nBirth Date: 20 October 2010.\nAge: 13 Years Old\nAddress: E-302 Officers Flat, Gulbai Tekra, Ahmedabad, Gujarat");    
+    
+    return 0;
+}
